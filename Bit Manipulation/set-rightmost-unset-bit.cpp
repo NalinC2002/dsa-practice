ac148c209e57d0ab2a08fa7e3175aadcf0f9405b
NalinC2002 <nalinchourasia10012002@gmail.com>
2024-01-30 17:12:54 +0530

    Added solution to Set rightmost bit problem

---
+++ Bit Manipulation/set-rightmost-unset-bit.cpp
@@ -0,0 +1,25 @@
+/*
+The problem is to find the rightmost bit of a non-negative number 'N' 
+that is currently unset (i.e., has a value of 0) in its binary 
+representation and set it to 1.
+
+Return the number after setting the rightmost unset bit of 'N'. 
+If there are no unset bits in N's binary representation, 
+then the number should remain unchanged.
+1 <= N <= 10^9
+
+Problem link - https://www.codingninjas.com/studio/problems/set-the-rightmost-unset-bit_8160456
+*/
+
+int setBits(int n){
+    // Write your code here.
+    int i=0;
+    int x = n;
+    while(x&1) {
+        i++;
+        x = x>>1;
+    }
+    if(n > (1<<i))
+        return n + (1<<i);
+    return n;    
+}
