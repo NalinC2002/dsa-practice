1d49dd38f68bfaf04b5fa02f51480604c7cfc63a
NalinC2002 <nalinchourasia10012002@gmail.com>
2024-01-31 20:37:10 +0530

    Added solution

---
+++ Arrays/check-sorted-array.cpp
@@ -0,0 +1,21 @@
+/*
+Given an array ‘a’ of ‘n’ non-negative integers, check whether 
+the given array is sorted in the non-decreasing order or not.
+Return 1 if the given array is sorted. Else, return 0.
+*/
+
+int isSorted(int n, vector<int> a) {
+    // Write your code here.
+    for(int i=0; i<n-1; i++){
+        if(a[i]>a[i+1]) return 0;    
+    }
+    return 1;
+}
+
+/*
+Approach:
+
+We traverse the array and check if current element is lesser
+than the next element. If not return 0.
+If we reach the end of loop, return true.
+*/
