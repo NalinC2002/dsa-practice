b41ce14033906c54310286a687f3ae85f1dc2db2
NalinC2002 <nalinchourasia10012002@gmail.com>
2024-02-01 13:24:23 +0530

    Modified solution

--- Arrays/remove-duplicates-from-sorted-array.cpp
+++ Arrays/remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ Given an integer array nums sorted in non-decreasing order, remove the duplicate
 such that each unique element appears only once. The relative order of the elements should be kept the same. 
 Then return the number of unique elements in nums.
 
-
+Problem link - https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 */
 
 
