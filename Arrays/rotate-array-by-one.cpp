9d8bc9d13b67e755db4de30c01e9155c4355fbd1
NalinC2002 <nalinchourasia10012002@gmail.com>
2024-02-02 11:22:50 +0530

    Added solution

---
+++ Arrays/rotate-array-by-one.cpp
@@ -0,0 +1,21 @@
+/*
+Given an array 'arr' containing 'n' elements, rotate this array left once and return it.
+Rotating the array left by one means shifting all elements by one place to the left and 
+moving the first element to the last position in the array.
+*/
+
+
+vector<int> rotateArray(vector<int>& arr, int n) {
+    // Write your code here.
+    arr.push_back(arr[0]);
+    arr.erase(arr.begin());
+
+    return arr;
+}
+
+
+/*
+Approach:
+
+Push first element at last of vector arr and then remove the first element of arr.
+*/
